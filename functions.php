e0f6c13285eee15b18151dabc34329e0b5e98441
DanteMaiden <david.bartholome70@hotmail.com>
2023-11-26 18:26:51 +0100

    add filters for homepage

--- functions.php
+++ functions.php
@@ -42,4 +42,102 @@ function motaphoto_enqueue_lightbox() {
 }
 
 add_action('wp_enqueue_scripts', 'motaphoto_enqueue_lightbox');
+
+
+// Charger plus de miniatures avec AJAX
+function load_more_photos() {
+
+    $page = $_POST['page'];
+    $format = isset($_POST['format']) ? $_POST['format'] : '';
+    $categorie = isset($_POST['categorie']) ? $_POST['categorie'] : '';
+    $order = isset($_POST['order']) ? $_POST['order'] : 'DESC';
+        // var_dump($page);
+        // var_dump($format);
+        // var_dump($categorie);
+        // var_dump($order);
+
+    if($categorie == 'all' && $format == 'all'){
+        $args = array(
+            'post_type' => 'photos',
+            'posts_per_page' => 12,
+            'paged' => $page,
+            'order' => $order,
+        );
+    }else if($categorie == 'all'){
+        $args = array(
+            'post_type' => 'photos',
+            'posts_per_page' => 12,
+            'paged' => $page,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'format',
+                    'field' => 'slug',
+                    'terms' => $format,
+                ),
+            ),
+            'order' => $order,
+        );
+    }else if($format== 'all'){
+        $args = array(
+            'post_type' => 'photos',
+            'posts_per_page' => 12,
+            'paged' => $page,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'categorie',
+                    'field' => 'slug',
+                    'terms' => $categorie,
+                ),
+            ),
+            'order' => $order,
+        );
+    }else{
+        $args = array(
+            'post_type' => 'photos',
+            'posts_per_page' => 12,
+            'paged' => $page,
+            'tax_query' => array(
+                'relation' => 'AND',
+                array(
+                    'taxonomy' => 'format',
+                    'field' => 'slug',
+                    'terms' => $format,
+                ),
+                array(
+                    'taxonomy' => 'categorie',
+                    'field' => 'slug',
+                    'terms' => $categorie,
+                ),
+            ),
+            'order' => $order,
+        );
+    }
+
+    var_dump($args);
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            // Affichez vos miniatures ici
+            echo get_the_post_thumbnail();
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'No more photos found';
+    endif;
+
+    die();
+}
+
+add_action('wp_ajax_load_more_photos', 'load_more_photos');
+add_action('wp_ajax_nopriv_load_more_photos', 'load_more_photos');
+
+// Enqueue le script JS et les styles nécessaires
+function enqueue_custom_scripts_and_styles() {
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/custom-script.js', array('jquery'), '', true);
+    wp_localize_script('custom-script', 'ajax_obj', array('ajaxurl' => admin_url('admin-ajax.php')));
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts_and_styles');
     
--- home.php
+++ home.php
@@ -26,6 +26,62 @@
 </div>
 
 <!-- Affichage du moteur de recherche d'images -->
+<!-- Dans votre fichier de template de page d'accueil -->
+
+<!-- Sélecteurs de filtres -->
+<div>
+    <label for="format-select">Format:</label>
+    <select id="format-select">
+        <!-- Remplacez 'format_1', 'format_2', etc., par vos termes de taxonomie réels -->
+        <option value="all">Tout</option>
+        <option value="portrait">Portrait</option>
+        <option value="paysage">Paysage</option>
+        <!-- Ajoutez d'autres options selon vos besoins -->
+    </select>
+
+    <label for="categorie-select">Catégorie:</label>
+    <select id="categorie-select">
+        <!-- Remplacez 'category_1', 'category_2', etc., par vos termes de taxonomie réels -->
+        <option value="all">Tout</option>
+        <option value="mariage">Mariage</option>
+        <option value="concert">Concert</option>
+        <!-- Ajoutez d'autres options selon vos besoins -->
+    </select>
+
+    <label for="order-select">Ordre:</label>
+    <select id="order-select">
+        <option value="ASC">Croissant</option>
+        <option value="DESC">Décroissant</option>
+    </select>
+</div>
+
+<!-- Conteneur pour les miniatures -->
+<div id="photos-container">
+    <?php
+    $args = array(
+        'post_type' => 'photos',
+        'posts_per_page' => 12,
+        'paged' => 1,
+        'order' => 'ASC',
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            // Affichez vos miniatures ici
+            echo get_the_post_thumbnail();
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'No photos found';
+    endif;
+    ?>
+</div>
+
+<!-- Bouton "Charger plus" -->
+<button id="load-more-photos">Charger plus</button>
+
 
 
 
