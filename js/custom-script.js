57883cab972400c50e3a7cc5be6787ffe6030b96
DanteMaiden <david.bartholome70@hotmail.com>
2023-12-01 12:42:10 +0100

    add custom filters

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,4 +1,72 @@
 jQuery(function($) {
+
+    //personnalisation des filtres
+
+    // iteration à travers chaque select
+    jQuery('select').each(function () {
+
+        // cache les options
+        var $this = $(this),
+            numberOfOptions = $(this).children('option').length;
+    
+            // cache les selects
+            $this.addClass('s-hidden');
+    
+            // entoure le select d'un div
+            $this.wrap('<div class="select"></div>');
+    
+            // ajout d'un div stylisé pour remplacer le select caché
+            $this.after('<div class="styledSelect"></div>');
+    
+            // affiche le div
+            var $styledSelect = $this.next('div.styledSelect');
+    
+            // affiche la première option dans le div stylisé
+            $styledSelect.text($this.children('option').eq(0).text());
+    
+            // ajoute une liste ul pour les options
+            var $list = $('<ul />', {
+                'class': 'options'
+            }).insertAfter($styledSelect);
+    
+            // ajoute les options dans les li
+            for (var i = 0; i < numberOfOptions; i++) {
+                $('<li />', {
+                    text: $this.children('option').eq(i).text(),
+                    rel: $this.children('option').eq(i).val()
+                }).appendTo($list);
+            }
+    
+            // affiche les li
+            var $listItems = $list.children('li');
+    
+            // affiche la liste des options quand le div select est cliqué et cache quand le select est à nouveau cliqué
+            $styledSelect.click(function (e) {
+                e.stopPropagation();
+                $('div.styledSelect.active').each(function () {
+                    $(this).removeClass('active').next('ul.options').hide();
+                });
+                $(this).toggleClass('active').next('ul.options').toggle();
+            });
+    
+            //cache la liste quand un item est selectionné
+            // met à jour la valeur selectionnée dans la liste
+            $listItems.click(function (e) {
+                e.stopPropagation();
+                $styledSelect.text($(this).text()).removeClass('active');
+                $this.val($(this).attr('rel'));
+                $list.hide();
+            });
+    
+            // cache la liste quand on clique en dehors de l'élément
+            $(document).click(function () {
+                $styledSelect.removeClass('active');
+                $list.hide();
+            });
+    
+        });
+
+    //script changement de filtre et charger plus
     var page = 2;
     var canLoadMore = true;
 
@@ -28,8 +96,8 @@ jQuery(function($) {
         }
     });
 
-    // Filtrer les photos lorsqu'un filtre est changé
-    $('#format-select, #categorie-select, #order-select').on('change', function() {
+     // Filtrer les photos lorsqu'un filtre est changé
+     $('.options li').on('click', function() {
         // Réinitialiser la pagination
         page = 1;
 
